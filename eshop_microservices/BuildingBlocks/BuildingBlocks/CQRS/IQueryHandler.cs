56309eb2b05e07ba8176e834ad16059c2bd88065
rishadislam1 <rishad.islam158@gmail.com>
2026-05-29 21:01:42 +0600

    added vertical slice architecture

---
+++ eshop_microservices/BuildingBlocks/BuildingBlocks/CQRS/IQueryHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface IQueryHandler<TQuery,TResponse>:
+        IRequestHandler<TQuery,TResponse>
+        where TQuery:IQuery<TResponse>
+        where TResponse: notnull
+    {
+    }
+}
